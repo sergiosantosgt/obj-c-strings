7dc912dd8fddd6a9c6efa2e1540b2bc0e54afb59
Sérgio Santos <sergiorobsantos@gmail.com>
2022-02-19 22:48:20 -0300

    Strings utilization

--- obj-c-strings/ViewController.h
+++ obj-c-strings/ViewController.h
@@ -7,8 +7,11 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    NSString *name;
+}
 
+@property (weak, nonatomic) IBOutlet UILabel *label;
 
 @end
 
--- obj-c-strings/ViewController.m
+++ obj-c-strings/ViewController.m
@@ -16,6 +16,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    name = @"Sergio";
+    
+    NSString *word = @"Hello";
+    
+    self.label.text = [NSString stringWithFormat:@"%@ %@", word, name];
 }
 
 
